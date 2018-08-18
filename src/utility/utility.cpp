71f0bd2e0160ced8684a42b62443ba78618363fa
Nix <57319138@qq.com>
2018-08-18 09:24:34 +0800

    testbed for time interval

--- src/utility/utility.cpp
+++ src/utility/utility.cpp
@@ -1,19 +1,64 @@
 #include "utility.h"
+#include <iostream>
+
+using namespace std;
 
 StopWatch::StopWatch()
 {
-    Start();
+    Reset();
 }
 
-void StopWatch::Start()
+void StopWatch::Reset()
 {
-    tp = steady_clock::now();
+    startTime = steady_clock::now();
+    intervals.clear();
+    intervalSum = 0;
+}
+
+float StopWatch::Tick()
+{
+    auto difT = steady_clock::now() - lastUpdateTime;
+    lastUpdateTime = now;
+    auto ms = toMs(difT);
+    intervals.push_back(ms);
+    recentlyIntervals.push(ms);
+    if(recentlyIntervals.size()>recentCount){
+        recentlyIntervals.pop();
+    }
+    intervalSum += ms;
+    cout<<ms<<"ms,"<<"mean:"<<ms/(float)intervals.size()<<endl;
+    return ms;
 }
 
 float StopWatch::Count()
+{   
+    auto difT = steady_clock::now() - startTime;
+    lastUpdateTime = now;
+    return (float)difT.count/1000000.0f;
+}
+
+void StopWatch::Print()
 {
-    auto lastUpdateTime = steady_clock::now();
-    auto difT = lastUpdateTime - tp;
-    auto ping = difT.count();
-    return (float)ping/1000000.0f;
+    cout<<"Stop Watch Interval Datas:"<<endl;
+    float sum = 0;
+    for(int i = 0;i<intervals.size();i++){
+        auto iv = intervals[i];
+        sum += iv;
+        cout<<iv<<"ms,"<<"mean:"<<iv/(float)(i+1)<<endl;
+    }
+}
+
+float StopWatch::RecentlyMean()
+{
+    float sum = 0;
+    for(int i = 0;i<recentlyIntervals.size();i++){
+        auto iv = recentlyIntervals[i];
+        sum += iv;
+    }
+    return sum/(float)recentlyIntervals.size();
+}
+
+float StopWatch::toMs(input)
+{
+    return (float)input.count/1000000.0f;
 }
--- src/utility/utility.h
+++ src/utility/utility.h
@@ -6,16 +6,27 @@
 #define STRING2JSON(X) QJsonDocument::fromJson(X.toLatin1()).object()
 
 #include <chrono>
+#include <list>
+#include <queue>
 using namespace std::chrono;
 
 class StopWatch
 {
 public:
     StopWatch();
-    void Start();
+    void Reset();
+    float Tick();
     float Count();
+    void Print();
+    float RecentlyMean();
 private:
-    time_point<steady_clock> tp;
+    float toMs(auto input);
+    time_point<steady_clock> startTime;
+    time_point<steady_clock> lastUpdateTime;
+    std::list<float> intervals;
+    std::queue<float> recentlyIntervals;
+    int recentCount = 10;
+    float intervalSum = 0;
 };
 
 #endif // UTILITY_H
